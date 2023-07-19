90a47c656a4239f3750cbcd4aa9fb3185c991cf5
akhil <akhilgajjala7@gmail.com>
2023-07-19 21:48:00 +0530

    reminder App

---
+++ app/src/main/java/com/example/rermainder/model.java
@@ -0,0 +1,39 @@
+package com.example.rermainder;
+//model class is used to set and get the data from database
+
+public class model {
+    String title, date, time;
+
+    public model() {
+    }
+
+    public model(String title, String date, String time) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
